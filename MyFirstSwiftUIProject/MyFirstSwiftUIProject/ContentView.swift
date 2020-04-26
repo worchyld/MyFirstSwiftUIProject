9bc66acded8b4ac7c1dcc3614f5022d38f4fff15
AD <silentflute.ad@gmail.com>
2020-04-26 01:00:22 +0100

    Sort the contacts

--- MyFirstSwiftUIProject/MyFirstSwiftUIProject/ContentView.swift
+++ MyFirstSwiftUIProject/MyFirstSwiftUIProject/ContentView.swift
@@ -11,12 +11,17 @@ import SwiftUI
 let contacts = [
     Contact(name: "Alex", jobTitle: "Head of Operations", phone: "555-123-456"),
     Contact(name: "Smith", jobTitle: "CTO", phone: "555-100-100"),
-    Contact(name: "Sarah", jobTitle: "Sales", phone: "555-400-900")
+    Contact(name: "Sarah", jobTitle: "Sales", phone: "555-400-900"),
+    Contact(name: "Helen", jobTitle: "Management", phone: "555-123-999")
 ]
+let sortedContacts = contacts.sorted { (a:Contact, b:Contact) -> Bool in
+    return (a.name < b.name)
+}
 
 struct ContentView: View {
+
     var body: some View {
-        List(contacts, id: \.name) { contact in
+        List(sortedContacts, id: \.name) { contact in
             ContactRow(contact: contact)
         }
     }
